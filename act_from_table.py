6bc7961ee151f139aef0d1baf26b7cf550d0e73d
Rustemhak <rustemhak@mail.ru>
2023-02-02 11:47:29 +0300

    add todo for exceptions

--- act_from_table.py
+++ act_from_table.py
@@ -23,9 +23,14 @@ def get_values_8_10(pdf_file):
     idx_values = [0, 1, 3, 4, 5]
     values_8_10 = []
     for i in idx_values:
+        #TODO проверять найден ли параметр
         values_8_10.append([tables[1][2][i].strip()])
 
     data_8_10 = dict(zip(FIELDS_8_10, values_8_10))
+    # таблица ПЕРФОРАЦИЯ, ОТКЛЮЧЕНИЕ ПЛАСТОВ
+    #TODO проверять найдена ли таблица на привычном месте
     table11_17 = tables[5]
-    table18_28 = tables[6] if len(tables) > 6 else []
+    # АСХОД МАТЕРИАЛОВ, ИСПОЛЬЗУЕМЫХ ПРИ РЕМОНТЕ
+    # TODO проверять найдена ли таблица на привычном месте
+    table18_28 = tables[6]
     return data_8_10, table11_17, table18_28
